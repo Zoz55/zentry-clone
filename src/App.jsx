65c018218ab6003e9891546fc489edb8a2af9d82
Zoz55 <fayezkassem3@gmail.com>
2025-02-12 03:29:13 +0200

    done with the Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Hero from "./components/Hero.jsx";
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      helloe
       <Hero />
+      <section className='z-0 min-h-screen bg-blue-500'></section>
     </main>
   );
 };
